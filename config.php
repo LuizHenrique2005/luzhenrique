81d24f26a3a47d345c4208972956d5bcc1c270f9
LuizHenrique2005 <140400617+LuizHenrique2005@users.noreply.github.com>
2024-06-16 15:39:17 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = ""; 
-$dbname = "concessionariaproj"; 
-$port = "3308"; //Eu troquei a porta de conexao do meu xampp
-
-
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
